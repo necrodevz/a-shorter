19c02b04c0c4af498181a94a25ae3d2e85a069df
Devroy Blake <necrodevz@gmail.com>
2020-01-21 22:13:38 -0500

    Have some stuff

--- src/App.js
+++ src/App.js
@@ -1,167 +1,70 @@
 import React from 'react';
 
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import Button from '@material-ui/core/Button';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Switch from '@material-ui/core/Switch';
-import Fab from '@material-ui/core/Fab';
-import Paper from '@material-ui/core/Paper';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-import FolderIcon from '@material-ui/icons/Folder';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import NavigationIcon from '@material-ui/icons/Navigation';
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container'
+import Fab from '@material-ui/core/Fab';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
+
+import Timekey from './outputs/timekey';
+import Market from './outputs/market';
+import Home from './outputs/home';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: '100%',
+    flexGrow: 1,
   },
-  table: {
-    minWidth: 700,
+  menuButton: {
+    marginRight: theme.spacing(2),
   },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    flexBasis: '33.33%',
-    flexShrink: 0,
-  },
-  secondaryHeading: {
-    fontSize: theme.typography.pxToRem(15),
-    color: theme.palette.text.secondary,
+  title: {
+    flexGrow: 1,
   },
 }));
 
-function ControlledExpansionPanels({date, billableHours, nonBillableHours, totalHours, children}) {
-
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleChange = panel => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
-
-  return (
-      <ExpansionPanel expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <TableCell rowSpan={3} />
-          <TableCell className={classes.heading} colSpan={2}>{date}</TableCell>
-          <TableCell className={classes.secondaryHeading}>{billableHours}</TableCell>
-          <TableCell className={classes.secondaryHeading}>{nonBillableHours}</TableCell>
-          <TableCell className={classes.secondaryHeading}>{totalHours}</TableCell>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          {children}
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-  );
-}
-
 function App() {
-  
-    const classes = useStyles();
-    const [value, setValue] = React.useState('recents');
-  
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
-
-    function a11yProps(index) {
-      return {
-        id: `action-tab-${index}`,
-        'aria-controls': `action-tabpanel-${index}`,
-      };
-    }
-
+  const classes = useStyles();
   return (
-    <Grid container spacing={3}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="action tabs example"
-        >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-        </Tabs>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            A-NET Client Enterprise Application
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
       </AppBar>
-      <Grid item md={12} xs={12}>
-        <TableContainer component={Paper}>
-          <Table className={classes.table} aria-label="spanning table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="center" colSpan={3}>
-                  Details
-                </TableCell>
-                <TableCell align="right">Price</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell />
-                <TableCell>Date</TableCell>
-                <TableCell align="right">Billable</TableCell>
-                <TableCell align="right">Unbillable</TableCell>
-                <TableCell align="right">Total Billable</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell><TableCell><Switch size="small" /></TableCell><TableCell></TableCell></TableCell>
-                <TableCell>{new Date().toDateString()}</TableCell>
-                <TableCell align="right">8.0015</TableCell>
-                <TableCell align="right">.75</TableCell>
-                <TableCell align="right">8</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Grid>
-      <Grid container item spacing={4} xs={12}>
-        <Grid item container md={3} xs={6}>
-          <Grid item xs md={6}>
-            <Paper>
-              <Button>Button1</Button>
-            </Paper>
-          </Grid>
-          <Grid item xs md={6}>
-          <Paper>
-            <Button>Button2</Button>
-          </Paper>
-          </Grid>
-        </Grid>
-        <Grid item md={6} xs={12} />
-        <Grid item md={3} xs={6}>
-          <Paper>
-            <Button>Button3</Button>
-          </Paper>
-        </Grid>
-      </Grid>
-      <Fab variant="extended" float="right">
-        <NavigationIcon className={classes.extendedIcon} />
-          Navigate
+      <Container fixed>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/timekey">
+              <Timekey />
+            </Route>
+            <Route path="/market">
+              <Market />
+            </Route>
+          </Switch>
+        </Router>
+      </Container>
+      <Fab color="secondary" size="small" style={{position: "absolute", bottom: '5px', right: '5px'}}>
+        <KeyboardArrowUpIcon />
       </Fab>
-    </Grid>    
+    </>
   );
 }
 
---
+++ src/outputs/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() 
+{
+    return (
+    <div>
+        <h2>Welcome to A-NEt Pay's Client Enterprise Application</h2>
+        <h4>Please log in to continue.</h4>
+    </div>
+    )
+}
