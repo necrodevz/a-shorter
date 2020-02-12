3fa837b35efa66f68d236e7583081fdbd3900812
Devroy Blake <kapil-dev@outlook.com>
2020-02-12 13:24:19 -0500

    fixed imports

--- src/outputs/onboarding/index.js
+++ src/outputs/onboarding/index.js
@@ -9,7 +9,6 @@ import StepContent from '@material-ui/core/StepContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
   root: {
